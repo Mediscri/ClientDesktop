5fb5a3c06e1794ef81843ac182a370977562c5d7
JWWon <wonjiwn@naver.com>
2018-07-31 18:27:43 +0900

    MED-63 import font-family, set basic style syntaxes

---
+++ src/styles/color.js
@@ -0,0 +1,12 @@
+export default {
+  white: '#FFFFFF',
+  black: '#121212',
+  green: '#429D51',
+  red: '#D14040',
+  yellow: '#F9C433',
+  light_gray: '#ABABAA',
+  lighter_gray: '#EDEEEC',
+  dark_gray: '#8C8C89',
+  darker_gray: '#707070',
+  gray_transparent: 'rgba(172, 172, 170, 0.68)',
+};
---
+++ src/styles/size.js
@@ -0,0 +1,83 @@
+const icon = {
+  // width
+  mediscri: {
+    big: '38rem',
+    small: '11.2rem',
+  },
+  close: '2.8rem',
+  search: '1.6rem',
+  dropdown: '1rem',
+  notice: {
+    off: '1.8rem',
+    on: '1.9rem',
+    click: '3rem',
+  },
+  patient_status: '1.2rem',
+  accuracy: '3rem',
+  loading: '1.6rem',
+  // height
+  repeat: '1.6rem',
+  default: '2rem',
+};
+
+const space = {
+  default: '3.4rem',
+  narrow: '1.6rem',
+  title_content: '6rem',
+  name_content: '1.2rem',
+  between_contents: '2.4rem',
+  icon_text: '2rem',
+  between_categroies: '2.8rem',
+};
+
+const container = {
+  // width
+  dashboard: '26.8rem',
+  modal_dropdown: '20rem',
+  // height
+  navbar: '8rem',
+  // both
+  modal_big: {
+    width: '75rem',
+    height: '77rem',
+  },
+  modal_info: {
+    width: '39rem',
+    height: '19.8rem',
+  },
+  context_menu: {
+    width: '20rem',
+    height: '14.4rem',
+  },
+};
+
+const wrapper = {
+  login: {
+    width: '40.7rem',
+  },
+  category: {
+    width: '37.5rem',
+    height: '42rem',
+  },
+};
+
+const element = {
+  login_hr: {
+    width: '4px',
+    height: '48.5rem',
+  },
+  login_input: {
+    width: wrapper.login.width,
+    height: '5.2rem',
+    border: '0.3rem',
+    radius: '0.8rem',
+  },
+  button_with_text: {
+    width: '20rem',
+    height: '3.5rem',
+    border: '0.2rem',
+    radius: '0.8rem',
+  },
+};
+
+export default { icon, space, wrapper, container, element };
