ef2b7f4b25a0c8c7e27b4aed6ad7efa5712918d8
JWWon <wonjiwn@naver.com>
2018-08-02 17:18:31 +0900

    MED-64 update WebSocket.js to class instance

--- src/networks/WebSocket.js
+++ src/networks/WebSocket.js
@@ -1,24 +1,51 @@
+// @flow
 const DEVELOPMENT = 'DEVELOPMENT';
 const PRODUCTION = 'PRODUCTION';
 
-function setEndpoint(mode) {
+function setBaseURL(mode: string) {
   switch (mode) {
     case PRODUCTION:
       return 'ws://https://api.mediscri.com/';
     case DEVELOPMENT:
     default:
-      return 'ws://localhost:8000/';
+      return 'ws://localhost:8000/chat/test/';
   }
 }
 
-export function connect(endpoint) {
-  try {
-    const baseURL = setEndpoint(DEVELOPMENT);
-    const socket = new WebSocket(baseURL + endpoint);
-    console.log(`Socket Status: ${socket.readyState}`);
+class Socket {
+  static instance = null;
+  callbacks = {};
 
-    return socket;
-  } catch (e) {
-    console.error(e);
+  static getInstance() {
+    if (!Socket.instance) {
+      Socket.instance = new Socket();
+    }
+    return Socket.instance;
+  }
+
+  constructor() {
+    this.socket = null;
+  }
+
+  connect() {
+    const path = setBaseURL(DEVELOPMENT);
+    this.socket = new WebSocket(path);
+
+    this.socket.onopen = () => {
+      console.log(`WebSocket open - ${path}`);
+    };
+    this.socket.onmessage = e => {
+      console.log(e.data);
+    };
+    this.socket.onerror = e => {
+      console.log(e.message);
+    };
+    this.socket.onclose = () => {
+      console.log(`WebSocket closed, try to reconnect.`);
+      // this.connect(endpoint);
+    };
   }
 }
+
+const SocketInstance = Socket.getInstance();
+export default SocketInstance;
