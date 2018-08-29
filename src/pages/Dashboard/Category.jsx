4e43e9b8f2e6d3ad448858a71ee37fb23036c2ba
JWWon <wonjiwn@naver.com>
2018-08-29 21:37:34 +0900

    add explination on dashborad category

--- src/pages/Dashboard/Category.jsx
+++ src/pages/Dashboard/Category.jsx
@@ -13,13 +13,13 @@ type Props = {
 
 class Category extends Component<Props> {
   title = {
-    cc: 'Chief Complaint',
-    pi: 'Present Illness',
-    pmh: 'Past Medical Histroy',
-    fh: 'Family History',
-    sh: 'Social History',
-    ros: 'Review Of System',
-    u: 'Undefined',
+    cc: 'Chief Complaint(주된 증상)',
+    pi: 'Present Illness(증상에 대한 자세한 설명)',
+    pmh: 'Past Medical Histroy(과거력)',
+    fh: 'Family History(가족력)',
+    sh: 'Social History(사회력)',
+    ros: 'Review Of System(진단에 힌트가 될 내용)',
+    u: 'Undefined(분류할 수 없음)',
   };
 
   render() {
